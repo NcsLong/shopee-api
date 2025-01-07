5aa13c5ee1e68d492cc067ba953a283780691ede
Ncs Long <vulongdragon2005@gmail.com>
2025-01-07 15:57:37 +0700

    update
    hello

---
+++ vti-shopee-api/src/main/java/VTI_shopee/vti_shopee_api/VtiShopeeApiApplication.java
@@ -0,0 +1,13 @@
+package VTI_shopee.vti_shopee_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtiShopeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtiShopeeApiApplication.class, args);
+	}
+
+}
---
+++ vti-shopee-api/src/test/java/VTI_shopee/vti_shopee_api/VtiShopeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package VTI_shopee.vti_shopee_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtiShopeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
